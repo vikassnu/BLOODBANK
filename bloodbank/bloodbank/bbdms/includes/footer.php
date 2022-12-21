d6baa363d6b37e678df7ffdbac4a0a6937959485
vikas kumar gupta <92871774+vikassnu@users.noreply.github.com>
2022-12-21 11:50:01 +0530

    Add files via upload

---
+++ bloodbank/bloodbank/bbdms/includes/footer.php
@@ -0,0 +1,11 @@
+  
+  
+  <footer class="py-5 bg-inverse">
+        <div class="container">
+            <p class="m-0 text-center text-white">Copyright &copy; BloodBank & SNU Donor Management System 2022</p>
+            <P class="m-0 text-left text-white">Address: Dadri,Gautam Buddha Nagar,Greater Noida</P>
+            <P class="m-0 text-left text-white">Pincode: 201314</P>
+            <a class="m-0 text-right text-white" href="https://www.google.com/maps/dir/28.5305409,77.5733936/Shiv+Nadar+University,+NH91,+Tehsil+Dadri,+Greater+Noida,+Uttar+Pradesh+201314/@28.5301558,77.5727555,17z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x390ceb4eaaaaaaab:0x321412756718874c!2m2!1d77.575363!2d28.5267298"><p>Map</p></a>
+        </div>
+        <!-- /.container -->
+    </footer>
